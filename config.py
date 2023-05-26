b5c2e214647f1da3dc2c3a3773796c9bd008e6d8
lizok <esillicher@gmail.com>
2023-05-26 11:00:01 +0300

    add config

---
+++ config.py
@@ -0,0 +1,10 @@
+BOT_TOKEN = ''
+
+AUTH_FILE = 'path/to/service_account.json'
+
+SHEET_ID = ''
+
+DEFAULT_SCOPES = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive',
+]
